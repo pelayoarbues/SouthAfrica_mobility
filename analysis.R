5bea4f62da0d99ac6975b002f466ba37a0237054
Pelayo Arbues <gonzalezpelayo@gmail.com>
2014-12-01 20:02:13 +0100

    R script init

---
+++ analysis.R
@@ -0,0 +1,24 @@
+###################                                             ###############
+###################                                             ###############
+                                # NHTS 2014#
+###################                                             ###############
+###################                                             ###############
+
+
+# Author: Pelayo G. Arbués <gonzalezpelayo@gmail.com>
+
+
+#                         + LOAD REQUIRED PACKAGES +
+#Require packages
+packages <- c("foreign")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+
+
+#                               READ DATA
+
+
+
+
+
+
+
